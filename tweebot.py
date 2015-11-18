c54ce06301b7fd1f5a0189097749aa99a3a4f188
Dhruv Kar <kar.dhruv@gmail.com>
2015-11-18 00:43:00 +0000

    refactor functions

--- tweebot.py
+++ tweebot.py
@@ -4,33 +4,32 @@ import time
 from datetime import datetime, date, timedelta
 from pytz import timezone
 import pytz
+import xml.etree.ElementTree as ET
 from config import *
 
 #api.update_status(status=raw_input("Enter tweet: "))
 
-# Get today's date (not datetime) with Pacific timezone information
-def date_today():
+# Get date  with Pacific timezone information. days_ago is an offset; with a value of 0, it gets you today's date; with a value of 1, it gets you yesterday's date.
+def get_date(days_ago=0):
     utc = pytz.utc
     pacific = timezone('US/Pacific')
     naive_today = datetime.today()
     utc_today = utc.localize(naive_today)
-    today = utc_today.astimezone(pacific).date()
-    return today
+    date = utc_today.astimezone(pacific).date() - timedelta(days=days_ago)
+    return date
 
 # Get all jobs posted today as a list
-def get_todays_jobs(url):
+def get_jobs(url, date):
     collection = requests.get(url).json()['results']['collection1']
     DATE_FORMAT = SEIA_DATEFORMAT
-    todays_jobs = []
-    #today's date in Pacific Timezone
-    t = date_today()
+    jobs = []
     for x in collection:
-        if datetime.strptime(x['seia_date']['text'], DATE_FORMAT).date() == t:
+        if datetime.strptime(x['seia_date']['text'], DATE_FORMAT).date() == date:
             try:
-                todays_jobs.append([x['seia_title']['text'], x['seia_title']['href'], x['seia_location']['text'], x['seia_company']['text']])
+                jobs.append([x['seia_title']['text'], x['seia_title']['href'], x['seia_location']['text'], x['seia_company']['text'], x['seia_date']['text']])
             except:
                 pass
-    return todays_jobs
+    return jobs
 
 # Authorize Twitter and return an API object that we can use to tweet and do other fun stuff with
 def authorize_twitter(consumer_key, consumer_secret, access_token, access_token_secret):
@@ -43,14 +42,27 @@ def authorize_twitter(consumer_key, consumer_secret, access_token, access_token_
 def form_tweets(job_list):
     tweets = []
     for x in job_list:
-        tweet = x[3] + " is looking for a " + x[0] + " in " + x[2] + ". Apply here: " + x[1]
+        short_url = shorten_url(x[1])
+        tweet = x[3] + " is looking for a " + x[0] + " in " + x[2] + ". Apply here: " + short_url
         tweets.append(tweet)
     return tweets
 
+# Use psbe.co to shorten urls. Parse through xml response and extract shortened URL.
+def shorten_url(tweet_url):
+    shorten = 'http://psbe.co/API.asmx/CreateUrl?real_url='
+    r = requests.get(shorten+tweet_url)
+    root = ET.fromstring(r.text)
+    shortened_url = root[3].text
+    return shortened_url
+
+
 
 api = authorize_twitter(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-tweets = form_tweets(get_todays_jobs(SEIA_URL))
+d = get_date()
+tweets = form_tweets(get_jobs(SEIA_URL, d))
 
+'''
 for tweet in tweets:
     api.update_status(status=tweet)
-    time.sleep(300)
+    time.sleep(10)
+'''
